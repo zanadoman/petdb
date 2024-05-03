cb2d7953ad7dc5834225b9eea4a16fefc3c68e73
zanadoman <domanseven@gmail.com>
2024-05-03 14:40:29 +0200

    .

--- login.php
+++ login.php
@@ -1,27 +1,26 @@
 <?php
+    session_start();
+
     $user = $_POST["user"];
     $pwd = $_POST["pwd"];
 
     $conn = new mysqli("localhost", "root", "12345678", "petdb");
     if ($conn->connect_error) {
+        http_response_code(500);
         die($conn->connect_error);
     }
 
     $result = $conn->query("SELECT password FROM users WHERE name LIKE '$user'"); 
     
     if ($result->num_rows === 0) {
-        $conn->close();
-        die("Hibás felhasználónév");
-    }
-    if ($result->fetch_assoc()["password"] !== $pwd) {
-        $conn->close();
-        die("Hibás jelszó");
+        $_SESSION["error"] = "Hibás felhasználónév";
+    } else if ($pwd !== $result->fetch_assoc()["password"]) {
+        $_SESSION["error"] = "Hibás jelszó";
+    } else {
+        $_SESSION["user"] = $user;
     }
 
     $conn->close();
-
-    session_start();
-    $_SESSION["user"] = $user;
 
     require_once "dashboard.php";
 ?>
--- register.php
+++ register.php
@@ -1,26 +1,25 @@
 <?php
+    session_start();
+
     $user = $_POST["user"];
     $pwd = $_POST["pwd"];
 
     $conn = new mysqli("localhost", "root", "12345678", "petdb");
     if ($conn->connect_error) {
+        http_response_code(500);
         die($conn->connect_error);
     }
 
     if ($conn->query("SELECT * FROM users WHERE name LIKE '$user'")->num_rows !== 0) {
-        $conn->close();
-        die("Foglalt felhasználónév");
-    }
-
-    if (!$conn->query("INSERT INTO users (name, password) VALUES ('$user', '$pwd')")) {
-        $conn->close();
-        die("Sikertelen regisztráció");
+        $_SESSION["error"] = "Foglalt felhasználónév";
+    } else if (!$conn->query("INSERT INTO users (name, password) VALUES ('$user', '$pwd')")) {
+        http_response_code(500);
+        die($conn->error);        
+    } else {
+        $_SESSION["user"] = $user;
     }
     
     $conn->close();
-
-    session_start();
-    $_SESSION["user"] = $user;
 
     require_once "dashboard.php";
 ?>
